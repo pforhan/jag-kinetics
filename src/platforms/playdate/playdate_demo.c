5fb5e7401c3262c4dd7165c8046920482e05e7c2
Patrick Forhan <github.com@muddyhorse.com>
2026-01-13 12:54:16 -0600

    add tethers to playdate demo

--- src/platforms/playdate/playdate_demo.c
+++ src/platforms/playdate/playdate_demo.c
@@ -2,7 +2,6 @@
 #include "ak_physics.h"
 #include "pd_api.h"
 
-
 static ak_world_t world;
 static PlaydateAPI *pd = NULL;
 
@@ -26,6 +25,22 @@ static int update(void *userdata) {
       int h = AK_FIXED_TO_INT(b->shape.bounds.aabb.height);
       pd->graphics->drawRect(x - w, y - h, w * 2, h * 2, kColorBlack);
     }
+  }
+
+  // Draw Tethers
+  for (int i = 0; i < world.tether_count; i++) {
+    ak_tether_t *t = &world.tethers[i];
+    if (t->a == NULL || t->b == NULL)
+      continue;
+
+    int x1 = AK_FIXED_TO_INT(t->a->position.x);
+    int y1 = AK_FIXED_TO_INT(t->a->position.y);
+    int x2 = AK_FIXED_TO_INT(t->b->position.x);
+    int y2 = AK_FIXED_TO_INT(t->b->position.y);
+
+    pd->graphics->drawLine(x1, y1, x2, y2, 1, kColorBlack);
+
+    // TODO: Draw curve when tether is slack (current_dist < length)
   }
 
   return 1;
